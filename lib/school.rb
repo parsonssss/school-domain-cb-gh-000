ca7c7e36bbd7c9fca1c79e42090aad0f3e24f3dd
Sam Json <3115002443@mail2.gdut.edu.cn>
2018-12-14 15:10:41 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,15 @@
 # code here!
+class School
+  def initialize name
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(name,grade)
+    @roster[grade] ||= []
+    @roster[grade] << name
+  end
+
+
+
+end
