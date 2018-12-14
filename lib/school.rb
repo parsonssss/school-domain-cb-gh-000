253fd174038725150e039c0099d7ca61932459ea
Sam Json <3115002443@mail2.gdut.edu.cn>
2018-12-14 15:54:03 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
 # code here!
+require 'pry'
+class School
+  attr_reader :roster
+  def initialize name
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(name,grade)
+    @roster[grade] ||= []
+    @roster[grade] << name
+  end
+
+  def grade gradenum
+    @roster[gradenum]
+  end
+
+  def sort
+    @roster.each do |key,value|
+      value.sort!
+    end
+    @roster.sort.to_h
+  end
+
+
+
+end
